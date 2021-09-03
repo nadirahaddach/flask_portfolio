6e231722a3782f19eea1a754124ba259df5336f9
nadirahaddach <89223568+nadirahaddach@users.noreply.github.com>
2021-09-03 13:24:17 -0700

    commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create a Flask instance
 app = Flask(__name__)
@@ -9,6 +9,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/greet/', methods=['GET', 'POST'])
+def greet():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("greet.html", name=name)
+    # starting and empty input default
+    return render_template("greet.html", name="World")
 
 
 # connects /kangaroos path to render kangaroos.html
