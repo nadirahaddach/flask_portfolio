815ded6fe5b2746f763676606e6545da57151fdc
paigemccartin <89223526+paigemccartin@users.noreply.github.com>
2021-09-21 14:18:56 -0700

    concepts page

--- main.py
+++ main.py
@@ -88,3 +88,22 @@ def prototype():
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
+
+from flask import Blueprint, render_template
+from algorithm.image import image_data
+
+starter_bp = Blueprint('starter', Paige,
+                       url_prefix='/starter',
+                       template_folder='templates',
+                       static_folder='static',
+                       static_url_path='assets')
+
+
+@starter_bp.route('/binary/')
+def binary():
+    return render_template("starter/binary.html")
+
+
+@starter_bp.route('/rgb/')
+def rgb():
+    return render_template('starter/rgb.html', images=image_data())
