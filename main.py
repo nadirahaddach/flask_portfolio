de6bd3635f3a5e2548347ea53026322f7c78b4c7
nadirahaddach <89223568+nadirahaddach@users.noreply.github.com>
2021-09-24 13:54:21 -0700

    nav bar change

--- main.py
+++ main.py
@@ -106,10 +106,10 @@ def nadirargb():
     return render_template('nadirargb.html', images=image_data())
 
 
-# runs the application on the development server
-if __name__ == "__main__":
-    app.run(debug=True)
-
 @app.route('/paigergb/')
 def paigergb():
     return render_template("paigergb.html", images=image_data())
+
+# runs the application on the development server
+if __name__ == "__main__":
+    app.run(debug=True)
