746c16f3bddf94ef08744bfc04427f2ff37aa5f2
paigemccartin <89223526+paigemccartin@users.noreply.github.com>
2021-11-01 12:40:34 -0700

    changes

---
+++ templates/lists.py
@@ -0,0 +1,9 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+print(len(thislist))
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
